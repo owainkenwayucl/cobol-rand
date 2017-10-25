d6f5db2c471c31af3591a5c7d1bf82ae3b6c9705
Owain Kenway <o.kenway@ucl.ac.uk>
2017-10-25 13:38:22 +0100

    Fortran comparison

--- plot.R
+++ plot.R
@@ -1,8 +1,17 @@
-# Assumes you have compiled genrand.cbl and > output to file "genrand.out"
-d <- read.csv("genrand.out")
+args <- commandArgs(trailingOnly=TRUE)
+
+infile <- "genrand.out"
+outfile <- "histogram.png"
+
+if (length(args)>1) {
+  infile <- args[1]
+  outfile <- args[2]
+}
+
+d <- read.csv(infile)
 
 # Open our output file
-png(file="histogram.png", width = 1280, height = 720)
+png(file=outfile, width = 1280, height = 720)
 
 # Plot.
 hist(as.numeric(d$r), breaks = 100, main = "Histogram of COBOL RANDOM function", xlab = "Value", col = "grey")
